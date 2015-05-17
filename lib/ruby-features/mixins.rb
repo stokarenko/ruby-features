46ba53a908517fd3201b5b348ce22a222fb4f9c0
Sergey Tokarenko <st@anahoret.com>
2015-05-17 21:32:34 +0300

    Refactoring.

--- lib/ruby-features/mixins.rb
+++ lib/ruby-features/mixins.rb
@@ -3,20 +3,23 @@ module RubyFeatures
     class << self
 
       def build_and_apply!(feature)
-        feature_name = feature.name
-        feature_module_name = RubyFeatures::Utils.modulize(feature_name)
+        feature_module = RubyFeatures::Utils.prepare_module!(
+          self,
+          RubyFeatures::Utils.modulize(feature.name)
+        )
 
-        RubyFeatures::Utils.prepare_module!(self, feature_module_name).class_eval do
-          feature.apply_to_blocks.each do |target, blocks|
-            RubyFeatures::Lazy.apply(target) do
-              RubyFeatures::Utils.prepare_module!(self, target).class_eval do
-                extend RubyFeatures::Concern
-                blocks.each { |block| class_eval(&block) }
-                _apply_to(target, feature_name)
-              end
-            end
+        feature.apply_to_blocks.each do |target, blocks|
+          RubyFeatures::Lazy.apply(target) do
+            _lazy_build_and_apply!(feature, feature_module, target, blocks)
           end
         end
+      end
+
+      def _lazy_build_and_apply!(feature, feature_module, target, blocks)
+        lazy_module = RubyFeatures::Utils.prepare_module!(feature_module, target)
+        lazy_module.extend RubyFeatures::Concern
+        blocks.each { |block| lazy_module.class_eval(&block) }
+        lazy_module._apply(target, feature.name)
       end
 
     end
