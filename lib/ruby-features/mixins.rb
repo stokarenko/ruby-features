fb21b8c82c257dc369f5c5e97fc3e3eeae383bac
Sergey Tokarenko <st@anahoret.com>
2015-05-17 12:49:07 +0300

    Refactor.

--- lib/ruby-features/mixins.rb
+++ lib/ruby-features/mixins.rb
@@ -3,20 +3,15 @@ module RubyFeatures
     class << self
 
       def build_and_apply!(feature)
-        feature_module_name = feature.name.classify
-
-        RubyFeatures::Utils.prepare_module!(self, feature_module_name).class_eval do
+        RubyFeatures::Utils.prepare_module!(self, feature.name.classify).class_eval do
           feature.include_in_blocks.each do |target, blocks|
             RubyFeatures::Utils.prepare_module!(self, target).class_eval do
               extend RubyFeatures::Concern
-
               blocks.each { |block| class_eval(&block) }
-
               _apply_to(target)
             end
           end
         end
-
       end
 
     end
